0502240a8b3e0bff27289f5a21a851548c63015c
adeshEupheus <104565905+adeshEupheus@users.noreply.github.com>
2022-05-25 10:51:02 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import grayBg from "../assets/grayBg.png";
+import logo from "../assets/logo.png";
+import menuLine from "../assets/menuLine.png";
+import { CircleOutlined, Circle } from "@mui/icons-material";
+
+const Navbar = () => {
+  return (
+    <div
+      className={`lg:px-16 md:px-4 gap-4 sm:px-16 px-6 py-4 flex flex-col md:flex-row items-center justify-between bg-[#dbdbdb] w-[100vw]`}
+      style={{ backgroundImage: `url(${grayBg})` }}
+    >
+      <img
+        src={logo}
+        className="md:w-auto md:h-[4rem] w-[10rem] h-[3rem]"
+        alt=""
+      />
+
+      <div
+        className="flex lg:gap-16 md:gap-8 sm:gap-16 gap-4 md:mr-12 mt-6 bg-no-repeat"
+        style={{ backgroundImage: `url(${menuLine})` }}
+      >
+        <div className=" flex flex-col   gap-1 justify-center items-center">
+          <Circle className="!text-[0.9rem] text-red-800" />
+          <span className="text-xs sm:text-base flex gap-1">
+            <span>Why</span>
+            <span>Eupheus</span>
+          </span>
+        </div>
+        <div className=" flex flex-col gap-1 justify-center items-center">
+          <CircleOutlined className="!text-[0.9rem]" />
+          <span className="text-xs sm:text-base flex gap-1">
+            <span>About</span>
+            <span>Us</span>
+          </span>
+        </div>
+        <div className=" flex flex-col gap-1 justify-center items-center">
+          <CircleOutlined className="!text-[0.9rem]" />
+          <span className="text-xs sm:text-base">Solutions</span>
+        </div>
+        <div className=" flex flex-col gap-1 justify-center items-center">
+          <CircleOutlined className="!text-[0.9rem]" />
+          <span className="text-xs sm:text-base">Demo</span>
+        </div>
+        <div className=" flex flex-col gap-1 justify-center items-center">
+          <CircleOutlined className="!text-[0.9rem]" />
+          <span className="text-xs sm:text-base">Events</span>
+        </div>
+        <div className=" flex flex-col gap-1 justify-center items-center">
+          <CircleOutlined className="!text-[0.9rem]" />
+          <span className="text-xs sm:text-base">Media</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
