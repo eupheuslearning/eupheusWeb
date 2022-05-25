05625ae96c7fae9676d66f60648116ae568de0cb
adeshEupheus <104565905+adeshEupheus@users.noreply.github.com>
2022-05-25 16:36:05 +0530

    responsive footer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -73,7 +73,7 @@ const Home = () => {
           <img key={1} className="w-[100vw]" src={Banner_2} alt="" />
           <img key={2} className="w-[100vw]" src={Banner_3} alt="" />
         </div>
-        <div className="flex gap-3 absolute w-full justify-center bottom-12">
+        <div className="flex gap-3 absolute w-full justify-center sm:bottom-12 bottom-4">
           <Circle
             key={0}
             onClick={() => setIndex(0)}
@@ -403,24 +403,28 @@ const Home = () => {
         />
       </div>
 
-      <div className="bg-red-600 w-[100vw] lg:px-[9rem] items-center px-[3rem] py-[2rem] flex justify-between ">
-        <div className="flex flex-col gap-3 w-[20vw]">
-          <img src={logWhite} className=" object-cover w-full" alt="" />
-          <span className="text-white text-sm w-full">
+      <div className="bg-red-600 w-[100vw] lg:px-[9rem] items-start sm:px-[3rem] px-4 py-[2rem] flex justify-between ">
+        <div className="flex flex-col gap-3 sm:w-[20vw] w-[25vw]">
+          <img src={logWhite} className="object-cover w-full" alt="" />
+          <span className="text-white sm:text-sm text-xs w-full">
             A-12, Second Floor, Mohan Cooperative Industrial Estate, Main
             Mathura Road, Near Sarita Vihar Metro Station, New Delhi – 110044
           </span>
         </div>
         <div className="flex flex-col gap-3">
-          <span className="text-white text-xl">USEFUL LINKS</span>
+          <span className="text-white sm:text-xl text-base">USEFUL LINKS</span>
           <div className="flex flex-col gap-1">
-            <span className="text-white text-sm">Properties</span>
-            <span className="text-white text-sm">New Books</span>
-            <span className="text-white text-sm">Awards & Recognitions</span>
-            <span className="text-white text-sm">News</span>
-            <span className="text-white text-sm">Press Release</span>
-            <span className="text-white text-sm">Terms And Conditions</span>
-            <span className="text-white text-sm">Career</span>
+            <span className="text-white sm:text-sm text-xs">Properties</span>
+            <span className="text-white sm:text-sm text-xs">New Books</span>
+            <span className="text-white sm:text-sm text-xs">
+              Awards & Recognitions
+            </span>
+            <span className="text-white sm:text-sm text-xs">News</span>
+            <span className="text-white sm:text-sm text-xs">Press Release</span>
+            <span className="text-white sm:text-sm text-xs">
+              Terms And Conditions
+            </span>
+            <span className="text-white sm:text-sm text-xs">Career</span>
           </div>
           <div className="flex gap-2">
             <Facebook />
@@ -430,26 +434,36 @@ const Home = () => {
           </div>
         </div>
         <div className="flex flex-col gap-3 w-[30vw]">
-          <span className="text-white text-xl">NEWSLETTER</span>
+          <span className="text-white sm:text-xl text-base">NEWSLETTER</span>
           <div className="flex flex-col gap-1">
-            <span className="text-white text-sm">
+            <span className="text-white sm:text-sm text-xs">
               Be the first to know about the latest news.
             </span>
             <input
               type="text"
               placeholder="E-mail...."
-              className=" bg-white px-4 py-1 w-[20vw] rounded-md outline-none"
+              className=" bg-white sm:px-4 px-2 placeholder:text-sm sm:placeholder:text-base sm:py-1 sm:w-[20vw] w-[30vw] rounded-md outline-none"
             />
           </div>
           <div className="flex gap-4 items-center w-full">
             <div className="flex flex-col gap-3 w-[12vw]">
-              <span className="text-white text-xl">GET IN TOUCH</span>
+              <span className="text-white sm:text-xl text-base w-[30vw]">
+                GET IN TOUCH
+              </span>
               <div className="flex flex-col gap-1">
-                <span className="text-white text-sm">+91 85265 25846</span>
-                <span className="text-white text-sm">info@eupheus.in</span>
+                <span className="text-white sm:text-sm text-xs w-[30vw]">
+                  +91 85265 25846
+                </span>
+                <span className="text-white sm:text-sm text-xs w-[30vw]">
+                  info@eupheus.in
+                </span>
               </div>
             </div>
-            <img src={phone} className="w-[15vw] h-auto" alt="" />
+            <img
+              src={phone}
+              className="w-[15vw] h-auto hidden sm:block"
+              alt=""
+            />
           </div>
         </div>
       </div>
