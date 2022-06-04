b39cec9a20813633f1c29eb322be27378d8ed761
adesh <me@mydomail.com>
2022-06-05 00:03:13 +0530

    nav fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1870px',
+      },
+    },
   },
   plugins: [],
 }
